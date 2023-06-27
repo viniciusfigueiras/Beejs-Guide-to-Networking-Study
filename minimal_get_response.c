6b24d60e988fcd659af123c4df9a9c0d1521ccba
Vinicius <viniciusfifer@gmail.com>
2023-06-27 13:29:30 -0300

    Update minimal_get_response.c

--- minimal_get_response.c
+++ minimal_get_response.c
@@ -7,11 +7,9 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
 
 #define PORT "80" // the port client will be connecting to 
-
 #define MAXDATASIZE 1024 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
